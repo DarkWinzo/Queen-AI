d0b6adef7f9e225ca3b0606480f350152864af67
DarkWinzo <darkwinzo4440@gmail.com>
2023-04-28 10:25:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,29 +1,26 @@
 const fs = require('fs')
 const chalk = require('chalk')
 
-
-
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-global.session = process.env.SESSION_ID || "";
+global.session = process.env.SESSION_ID || "PUT HERE"; //enter your session id here
 
-global.openaikey = process.env.OPEN_AI_KEY || "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"; //Put Your Open AI Api key
+global.openaikey = process.env.OPEN_AI_KEY || "ENTER_API-KEY_HERE"; //Put Your Open AI Api key
 
-global.ownername = process.env.OWNER_NAME || "DarkWinzo";
+global.ownername = process.env.OWNER_NAME || "DarkWinzo"; //Queen-AI Owner Name here
 
-global.ownernumber = process.env.OWNER_NUMBER || "94775200935";
+global.ownernumber = process.env.OWNER_NUMBER || "94775200935"; //Owner whatsapp number here
 
-global.language = "SI"; //Enter bot working language EN = English / SI = Sinhala /ML = Malay
+global.language = "EN"; //Enter bot working language EN = English / SI = Sinhala /ML = Malay
 
 global.prefix = ['.'] //Don't Change
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
 	fs.unwatchFile(file)
-	console.log(chalk.redBright("Update - 'config.js'"))
+	console.log(chalk.redBright("Update config.js"))
 	delete require.cache[file]
 	require(file)
 })
