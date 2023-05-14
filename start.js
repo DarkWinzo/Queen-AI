ce6b83df60eadbc169ab543bcb23d0b53768f48a
Dark-Ishuu <104496630+Dark-Ishuu@users.noreply.github.com>
2023-05-14 12:32:23 +0530

    Update start.js

--- start.js
+++ start.js
@@ -1,3 +1,20 @@
+/***
+                                ╭━━━┳╮ ╭┳━━━┳━━━┳━╮ ╭╮    ╭━━━┳━━╮
+                                ┃╭━╮┃┃ ┃┃╭━━┫╭━━┫┃╰╮┃┃    ┃╭━╮┣┫┣╯
+                                ┃┃ ┃┃┃ ┃┃╰━━┫╰━━┫╭╮╰╯┃    ┃┃ ┃┃┃┃
+                                ┃┃ ┃┃┃ ┃┃╭━━┫╭━━┫┃╰╮┃┃╭━━╮┃╰━╯┃┃┃
+                                ┃╰━╯┃╰━╯┃╰━━┫╰━━┫┃ ┃┃┃╰━━╯┃╭━╮┣┫┣╮
+                                ╰━━╮┣━━━┻━━━┻━━━┻╯ ╰━╯    ╰╯ ╰┻━━╯
+                                   ╰╯
+## Project Name => Queen-AI
+## Project Version => 1.0.0  
+## Project Author => DarkWinzo
+## Project license => MIT
+## Project Team => DarkSide Team
+## Module Name => Start.js
+## Thank For DarkWinzo
+***/ 
+
 var forever = require("forever-monitor");
 var child = new forever.Monitor("Connect/module/cilent.js", {
     silent: false,
