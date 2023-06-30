a40b54a7ee0d69befc55b6ce9235e73829006c3f
DarkWinzo <darkwinzo4440@gmail.com>
2023-06-30 17:41:27 +0530

    Update cilent.js

--- Connect/module/cilent.js
+++ Connect/module/cilent.js
@@ -24,7 +24,7 @@ const { default: QueenConnect,
        prepareWAMessageMedia, 
        generateWAMessageFromContent, 
        generateMessageID, 
-       makeCacheableSignalKeyStore, //Imlossible
+       makeCacheableSignalKeyStore,
        downloadContentFromMessage, 
        makeInMemoryStore, 
        jidDecode, 
