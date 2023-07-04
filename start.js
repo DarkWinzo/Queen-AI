b8056c1521187be99c13fdc760eb5d86442dbd41
DarkWinzo <darkwinzo4440@gmail.com>
2023-07-04 23:25:15 +0530

    Update start.js

--- start.js
+++ start.js
@@ -1,4 +1,4 @@
-/***
+/*
                                 ╭━━━┳╮ ╭┳━━━┳━━━┳━╮ ╭╮    ╭━━━┳━━╮
                                 ┃╭━╮┃┃ ┃┃╭━━┫╭━━┫┃╰╮┃┃    ┃╭━╮┣┫┣╯
                                 ┃┃ ┃┃┃ ┃┃╰━━┫╰━━┫╭╮╰╯┃    ┃┃ ┃┃┃┃
@@ -13,7 +13,7 @@
 ## Project Team => DarkSide Team
 ## Module Name => Start.js
 ## Thank For DarkWinzo
-***/ 
+*/ 
 
 var forever = require("forever-monitor");
 var child = new forever.Monitor("Connect/module/cilent.js", {
