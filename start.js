21bfd4e146cac412eba90be550510e46af29d8c1
DarkWinzo <darkwinzo4440@gmail.com>
2023-07-06 12:43:54 +0530

    Update start.js

--- start.js
+++ start.js
@@ -17,8 +17,7 @@
 
 var forever = require("forever-monitor");
 var child = new forever.Monitor("Connect/module/cilent.js", {
-//  silent: false,
-    silent: true,
+    silent: false,
     args: []
     });
 child.start();
