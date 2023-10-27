1b3aa21525cb5a5de0c59c5f88f6991773686eb6
DarkWinzo <darkwinzo4440@gmail.com>
2023-10-27 23:14:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@
                                 ╰━━╮┣━━━┻━━━┻━━━┻╯ ╰━╯    ╰╯ ╰┻━━╯
                                    ╰╯
 ## Project Name => Queen-AI
-## Project Version => 1.0.0  
+## Project Version => 1.0.1  
 ## Project Author => DarkWinzo
 ## Project license => MIT
 ## Project Team => DarkSide Team
@@ -18,22 +18,16 @@
 const fs = require('fs');
 const chalk = require('chalk');
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-global.session = process.env.SESSION_ID || "PUT HERE"; //enter your session id here
-
-global.openaikey = process.env.OPEN_AI_KEY || "ENTER_API-KEY_HERE"; //Put Your Open AI Api key
-
-global.ownername = process.env.OWNER_NAME || "DarkWinzo"; //Queen-AI Owner Name here
-
-global.ownernumber = process.env.OWNER_NUMBER || "94XXXXXXX"; //Owner whatsapp number here
-
-global.language = process.env.LANGUAGE || "EN"; //Enter bot working language EN = English / SI = Sinhala /ML = Malayalam /HI =Hindi /UR = Urdu
-
-global.prefix = ['.'] //Don't Change
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+module.exports = {
+      BASE_URL: process.env.BASE_URL || "https://queen-api-production.up.railway.app", //Queen-AI Base API Url
+      SESSION_ID: process.env.SESSION_ID || "QUEEN-AI;;;MemphisSnore", //enter your session id here
+      OWNER_NUMBER: process.env.OWNER_NUMBER || "94775200935", //Owner whatsapp number here
+      OWNER_NAME: process.env.OWNER_NAME || "DarkWinzo", //Queen-AI Owner Name here
+      PREFIX: ".",
+      LANGUAGE: process.env.LANGUAGE || "SI" //Enter bot working language EN = English / SI = Sinhala /ML = Malayalam /HI =Hindi /UR = Urdu
+};
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
 	fs.unwatchFile(file)
